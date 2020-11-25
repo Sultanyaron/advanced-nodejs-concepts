db11080e8e21571de5f732728222c75ac0fae828
sultanyaron <sultanyaron@gmail.com>
2020-11-25 11:31:50 +0200

    configuring ci

--- config/dev.js
+++ config/dev.js
@@ -5,4 +5,5 @@ module.exports = {
   mongoURI:
     "mongodb://yaronsultan:jxcdYNebzCZ0CQQ3@cluster0-shard-00-00.21hfp.mongodb.net:27017,cluster0-shard-00-01.21hfp.mongodb.net:27017,cluster0-shard-00-02.21hfp.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-rgqv7d-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
 };
